5d6aa938bc103857a89b69010e8d8c528248d52a
smeekas <smeetapatel008@gmail.com>
2022-10-31 16:11:37 +0530

    feature: primary and ghost varient of button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
-
+import React, { MouseEventHandler } from 'react';
+import Button from './components/Button/Button';
 
 
 function App() {
+  const ref=React.useRef<HTMLButtonElement>(null);
+  const handler:MouseEventHandler=(e)=>{
+    // console.log(ref)
+  }
   return (
-    <div className="App">hello</div>
+    <div className="App">
+    <div >hello</div>
+    <Button ghost  onClick={handler} varient="primary" />
+    </div>
   );
 }
 
 export default App;
-//TODO circle CI
 //TODO COLOR VARIABLES
 //TODO button component
+ //TODO --> button default design
+ //TODO --> button varient design design
+ //TODO --> button icon support 
+
+
+ 
