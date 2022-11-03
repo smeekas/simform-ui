4933593e40ff9766fab74d3600c50be293be835c
Shyam dadhaniya <shyam.dadhaniya04@gmail.com>
2022-11-03 17:26:39 +0530

    feature: Divider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
-import React, { MouseEventHandler } from 'react';
-import Button from './components/Button/Button';
-
+import React, { MouseEventHandler } from "react";
+import Button from "./components/Button/Button";
+import Divider from "./components/Divider/Divider";
 
 function App() {
-  const ref=React.useRef<HTMLButtonElement>(null);
-  const handler:MouseEventHandler=(e)=>{
+  const ref = React.useRef<HTMLButtonElement>(null);
+  const handler: MouseEventHandler = (e) => {
     // console.log(ref)
-  }
+  };
   return (
     <div className="App">
-    <div >hello</div>
-    <Button size='large'    ghost onClick={handler} varient="primary" />
-    <Button size='medium'   ghost onClick={handler} varient="primary" />
-    <Button     onClick={handler} varient="dashed" />
+      <div>hello</div>
+      <Button size="large" ghost onClick={handler} varient="primary" />
+      <Button size="medium" ghost onClick={handler} varient="primary" />
+      <Button onClick={handler} varient="dashed" />
+      <Divider></Divider>
+      <Divider varient="dashed"></Divider>
+      <Divider orientation="left">hello</Divider>
+      <Divider orientation="center">hello</Divider>
+      <Divider orientation="right">Hello</Divider>
+      <Divider varient="dashed" orientationMargin={20} orientation="left">
+        hello
+      </Divider>
+      <Divider varient="dashed">hello</Divider>
+      <Divider varient="dashed" orientationMargin={50} orientation="right">
+        hello
+      </Divider>
+      hello
+      <Divider type="vertical"></Divider>
+      Hello World!
+      <Divider type="vertical"></Divider>
+      {/* <Divider type="horizontal">rgt</Divider> */}
     </div>
   );
 }
@@ -20,9 +37,6 @@ function App() {
 export default App;
 //TODO COLOR VARIABLES
 //TODO button component
- //TODO --> button default design
- //TODO --> button varient design design
- //TODO --> button icon support 
-
-
- 
+//TODO --> button default design
+//TODO --> button varient design design
+//TODO --> button icon support
