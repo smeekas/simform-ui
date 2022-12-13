30a688cc4dbcba707cf94f9745c89fc6d7190bf2
smeekas <smeetapatel008@gmail.com>
2022-12-13 17:00:57 +0530

    wip

--- src/components/AvatarGroup/AvatarGroup.tsx
+++ src/components/AvatarGroup/AvatarGroup.tsx
@@ -1,18 +1,52 @@
 import React, { ReactNode } from 'react'
-interface AvatarGroupPropsType {
-    children: ReactNode[],
+import Avatar from '../Avatar/Avatar';
+import './AvatarGroup.scss'
+export interface AvatarGroupPropsType {
+    children?: ReactNode[],
     max?: number,
+    spacing?: "medium" | "small"
 
 }
-function AvatarGroup(props: AvatarGroupPropsType) {
-    const { children: unfilteredChildren, max = 3 } = props;
+const AvatarGroup = (props: AvatarGroupPropsType) => {
+    const { children: unfilteredChildren, max = 3, spacing = "small" } = props;
     const children = React.Children.toArray(unfilteredChildren).filter(child => {
         return React.isValidElement(child)
-    })
+    }) as JSX.Element[]
     const childrenCount = children.length;
-    const maxCount = childrenCount < max ? 0 : childrenCount - max;
+    const spacingOfAvatar = {
+        "medium": -10,
+        "small": -20,
+
+    }
+    const { size } = children[0].props
+    // console.log(style);
+    const maxCount = childrenCount < max ? childrenCount : max;
+    const extraAvatar = childrenCount - max;
     return (
-        <div>AvatarGroup</div>
+        <div className='zen-avatargroup'>
+            {children.slice(0, maxCount).map((child, index) => {
+                return React.cloneElement(child, {
+                    ...child.props,
+                    key: index,
+                    style: {
+                        posiion: "relative",
+                        display: "inline-flex",
+                        border: "2px solid #fff",
+                        // overflow:"hidden",
+                        marginLeft: `${index !== 0 ? (spacingOfAvatar[spacing]) : 0}px`
+                    }
+                })
+            })}
+            <Avatar size={"default"}
+                name={`+${extraAvatar}`}
+                style={
+                    {
+                        position: "relative",
+                        display: "inline-flex",
+                        border: "2px solid #fff",
+                        marginLeft: `${spacingOfAvatar[spacing]}px`
+                    }} />
+        </div>
     )
 }
 
--- src/stories/AvatarGroup.stories.tsx
+++ src/stories/AvatarGroup.stories.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import AvatarGroup from "../components/AvatarGroup/AvatarGroup";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-
+import Avatar from '../components/Avatar/Avatar'
 export default {
     title: "AvatarGroup",
     component: AvatarGroup,
@@ -16,5 +16,28 @@ const Template: ComponentStory<typeof AvatarGroup> = (args) => <AvatarGroup {...
 
 export const Avatar_with_Image = Template.bind({});
 Avatar_with_Image.args = {
+    children: [
+        <Avatar color="red" size="default" source="https://t4.ftcdn.net/jpg/03/64/21/11/240_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg" />,
+        <Avatar color="blue" size="default" alt="B" />
+        ,
+        <Avatar color="green" size="default" alt="C" />
+        ,
+        <Avatar color="purple" size="default" alt="D" />, < Avatar color="red" size="default" source="https://t4.ftcdn.net/jpg/03/64/21/11/240_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg" />,
+        <Avatar color="blue" size="default" alt="B" />
+        ,
+        <Avatar color="green" size="default" alt="C" />
+        ,
+        <Avatar color="purple" size="default" alt="D" />, <Avatar color="red" size="default" source="https://t4.ftcdn.net/jpg/03/64/21/11/240_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg" />,
+        <Avatar color="blue" size="default" alt="B" />
+        ,
+        <Avatar color="green" size="default" alt="C" />
+        ,
+        <Avatar color="purple" size="default" alt="D" />, <Avatar color="red" size="default" source="https://t4.ftcdn.net/jpg/03/64/21/11/240_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg" />,
+        <Avatar color="blue" size="default" alt="B" />
+        ,
+        <Avatar color="green" size="default" alt="C" />
+        ,
+        <Avatar color="purple" size="default" alt="D" />
 
+    ]
 };
